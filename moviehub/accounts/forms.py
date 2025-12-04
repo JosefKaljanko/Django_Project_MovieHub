a487165d5595474ed84d08b7e120be71f21a0527
Josef <josef.kaljanko@gmail.com>
2025-12-04 19:28:55 +0100

    oprava forms.py a accounts/views.py, bootstrap na accounts/login.html a accounts/profile.html

--- moviehub/accounts/forms.py
+++ moviehub/accounts/forms.py
@@ -1,10 +1,8 @@
-from os.path import altsep
-
 from django import forms
 # login
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
-from django.template.context_processors import request
+
 
 from .models import UserProfile
 from django.core.validators import EmailValidator
@@ -22,15 +20,18 @@ from django.core.validators import EmailValidator
 #         label="Heslo:",
 #         # strip=False,
 #     )
-# bootstrap
+
+# -----> LOGIN FORM bootstrap <-----
+
 class CustomLoginForm(AuthenticationForm):
     username = forms.CharField(
-        widget=forms.TextInput(attrs={
-            'class': 'form-control w-75',
-            'id': 'floatingUsername',
-            "placeholder": "Username"}), # musi byt pro bootstrap
         label="Uživatelské Jméno:",
-        # label="username",
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'id': 'floatingUsername',
+            "placeholder": "Uživatelské Jméno:",  # musi byt pro bootstrap
+            "autocomplete": "username",
+        }),
     )
     password = forms.CharField(
         label="Heslo:",
@@ -38,63 +39,96 @@ class CustomLoginForm(AuthenticationForm):
             'class': 'form-control',
             'id': 'floatingPassword',
             "placeholder": "Password", # musí být pro bootstrap!!!
-        },
-                                   # render_value=False
-                                   ),
-        # strip=False,
+            "autocomplete": "current-password",
+        }),
     )
 
-class CustomProfileEditForm(forms.ModelForm):
-    username = forms.CharField(max_length=50,
-                               label="Uživatelské Jméno",
-                               widget=forms.TextInput(attrs={
-                                   'class': 'form-control',
-                                   'id': 'floatingUsername',
-                                   "placeholder": "Username",
-                               }),
+# -----> EDIT USER (username, email) <-----
 
-                               help_text="Zde zadejte vaše uživatelske jméno.")
+class CustomProfileEditForm(forms.ModelForm):
+    username = forms.CharField(
+        max_length=50,
+        label="Uživatelské Jméno",
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'id': 'floatingUsername',
+            "placeholder": "Username",
+        }),
+        # help_text="Zde zadejte vaše uživatelske jméno."
+    )
+
+    email = forms.EmailField(
+        label="Email",
+        required=False,
+        help_text="Zadejte platný Email (Volitelné).",
+        widget=forms.EmailInput(attrs={
+            "class": "form-control",
+            "id": "floatingEmail",
+            "placeholder": "name@example.com",
+        }),
+    )
     # email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-control'}),
     #                          validators=[EmailValidator()],
     #                          help_text="Zde zadejte váš platný email.")
 
-    bio = forms.CharField(label="O vás:",
-                          widget=forms.Textarea(attrs={
-                              'class': 'form-control',
-                              'id': 'floatingBio',
-                              'rows': 4,
-                              "cols": 60
-                          }),
-
-                            help_text="Zde zadejte stručný popis o vás.",)
+    # BIO NENI V MODELU USER!!!
+    # bio = forms.CharField(label="O vás:",
+    #                       widget=forms.Textarea(attrs={
+    #                           'class': 'form-control',
+    #                           'id': 'floatingBio',
+    #                           'rows': 4,
+    #                           "cols": 60
+    #                       }),
+    #
+    #                         help_text="Zde zadejte stručný popis o vás.",)
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'bio')
+        fields = ('username', 'email')
         template_name = 'accounts/profile_edit.html'
+        # skryje def dlouhy helptex od django username
+        help_texts = {
+            "username": ""
+        }
 
 
 
 
-
+# -----> EDIT PROF bio, avatar <-----
 # TODO udelat upravu profilu username -> model:1; bio a avatar -> model:2
 class CusProfEditForm(forms.ModelForm):
 
-    username = forms.CharField(max_length=50,
-                               widget=forms.TextInput(attrs={'class': 'form-control', 'value': 'request.user'}),
-                               label="Nové Uživatelské Jméno",
-                               help_text="<br>Zde zadejte vaše uživatelske jméno. <br>")
+    # username = forms.CharField(max_length=50,
+    #                            widget=forms.TextInput(attrs={'class': 'form-control', 'value': 'request.user'}),
+    #                            label="Nové Uživatelské Jméno",
+    #                            help_text="<br>Zde zadejte vaše uživatelske jméno. <br>")
     #
-    # bio = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 4, "cols": 60}),
-    #                       label="O vás:",
-    #                       help_text="Zde zadejte stručný popis o vás.",
+    bio = forms.CharField(
+        label="O vás:",
+        required=False,
+        help_text="Zde zadejte stručný popis o vás.",
+        widget=forms.Textarea(attrs={
+            'class': 'form-control',
+            'id': 'floatingBio',
+            'rows': 4,
+            # "cols": 60
+            "placeholder": "Napište něco o sobě...",
+        }),
+    )
+
     #
     #                       )
     #
-    avatar2 = forms.ImageField(required=False, label="Obrázek",
-                               widget=forms.FileInput(attrs={'class': 'form-control'}))
+    avatar = forms.ImageField(
+        required=False,
+        label="Profilový Obrázek",
+        widget=forms.FileInput(attrs={
+            'class': 'form-control'
+        }),
+    )
 
     class Meta:
         model = UserProfile
-        # exclude = ('user',)
-        fields = '__all__'
+        exclude = ('user',)
+        # alternativne
+        # fields = ("bio", "avatar")
--- moviehub/accounts/views.py
+++ moviehub/accounts/views.py
@@ -1,5 +1,6 @@
 # from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
+from django.utils.decorators import method_decorator
 from django.views import View
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
@@ -16,6 +17,7 @@ from .models import User
 class RegisterView(View):
     """registrování nového uživatele."""
     def get(self,request):
+        # pozdeji form bootstrap
         return render(request, "accounts/register.html")
 
     def post(self,request):
@@ -26,13 +28,19 @@ class RegisterView(View):
 
         if password != password2:
             messages.error(request, "Hesla se neshodují!!!")
-            return render(request, "accounts/register.html")
+            context = {"username": username, "email": email}
+            return render(request, "accounts/register.html", context)
 
         if User.objects.filter(username=username).exists():
             messages.error(request, "Uživatelské jméno již existuje!!!")
-            return render(request, "accounts/register.html")
+            context = {"username": username, "email": email}
+            return render(request, "accounts/register.html", context)
 
-        user = User.objects.create_user(username=username, email=email, password=password)
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=password
+        )
         login(request, user)
         messages.success(request, "Registrace proběhla úspěšně.")
         return redirect("all_movies")
@@ -41,15 +49,14 @@ class RegisterView(View):
 def logout_view(request):
     """Odhlaseni uzivatele"""
     logout(request)
-    messages.info(request, "Logged out.")
+    messages.info(request, "Byl jsi úspěšně odhlášen.")
     return redirect("all_movies")
 
 @login_required
 def profile(request):
+    """Detail profilu přihlášeného uživatele."""
     context = {"user": request.user}
-    if request.method == "GET":
-        # return render(request, "accounts/profile.html",{"user": request.user})
-        return render(request, "accounts/profile.html",context)
+    return render(request, "accounts/profile.html",context)
 
 # class ProfileEditView(LoginRequiredMixin, UpdateView):
 #     model = User
@@ -57,37 +64,65 @@ def profile(request):
 #     template_name =
 
 class CustomProfileEdit(View):
-    def get(self,request):
+    def get(self, request):
         form = CustomProfileEditForm(instance=request.user)
         context = {"form":form}
         return render(request, "accounts/profile_edit.html",context)
+
     def post(self,request):
-        username = request.POST.get("username")
-        # email = request.POST.get("email")
-        bio = request.POST.get("bio")
-        print(username)
-        # print(email)
-        print(bio)
-        if User.objects.filter(username=username).exclude(pk=request.user.pk).exists():
-            print(f"{username} is already registered.")
-            return render(request, "accounts/profile_edit.html",context={"form":CustomProfileEditForm(), "mes":"Username už existuje!!"})
+        # username = request.POST.get("username")
+        # bio = request.POST.get("bio")
+        # print(username)
+        # print(bio)
+        # if User.objects.filter(username=username).exclude(pk=request.user.pk).exists():
+        #     print(f"{username} is already registered.")
+        #     return render(request, "accounts/profile_edit.html",context={"form":CustomProfileEditForm(), "mes":"Username už existuje!!"})
+        #
+        # conttext = {"mes":"dekujeme, data máme, pracujeme na tom..."}
+        # return render(request, "accounts/profile_edit.html",conttext)
 
-        conttext = {"mes":"dekujeme, data máme, pracujeme na tom..."}
-        return render(request, "accounts/profile_edit.html",conttext)
+        form = CustomProfileEditForm(
+            request.POST,
+            # request.FILES,
+            instance=request.user
+        )
 
-class CustomProfileEdit2(View):
-    def get(self,request):
-        # form = CustomProfileEditForm(instance=request.user)
-        form = CusProfEditForm(instance=request.user)
+        if form.is_valid():
+            # kontrola unikat. username idelane ve form/clean()
+            form.save()
+            messages.success(request, "Profil (\"jméno\" a \"email\" byl uspěšně upraven .")
+            return redirect("profile")
+
+        # pokud form ne-ok zustanou chyby + puvodni data
         context = {"form":form}
         return render(request, "accounts/profile_edit.html",context)
-    def post(self,request):
-        username = request.POST.get("username")
-        # email = request.POST.get("email")
-        bio = request.POST.get("bio")
-        print(username)
-        # print(email)
-        print(bio)
 
-        conttext = {"mes":"dekujeme, data máme, pracujeme na tom..."}
-        return render(request, "accounts/profile_edit.html",conttext)
+@method_decorator(login_required, name="dispatch")    # ??????
+class CustomProfileEdit2(View):
+    """
+    druha varianta upravy prof. - jiny form(avatar,bio...)
+    """
+    def get(self, request):
+        # form = CustomProfileEditForm(instance=request.user)
+        form = CusProfEditForm(instance=request.user.userprofile)
+        context = {"form":form}
+        return render(request, "accounts/profile_edit.html",context)
+
+    def post(self, request):
+        # username = request.POST.get("username")
+        # bio = request.POST.get("bio")
+        # print(username)
+        # print(bio)
+        form = CusProfEditForm(
+            request.POST,
+            request.FILES,
+            instance=request.user.userprofile,
+        )
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Profil \"bio\" a \"avatar\" byl uspěšně upraven (ver.2.0).")
+            return redirect("profile")
+
+        context = {"form": form}
+        return render(request, "accounts/profile_edit.html", context)
